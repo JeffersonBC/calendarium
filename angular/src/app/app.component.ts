ce04b3d9d3c43ddb02ae7798ccff3da323f864cf
Jefferson Boldrin Cardozo <jefferson.boldrin@gmail.com>
2018-03-07 14:23:07 -0300

    Adicionar serviço de frontend com o Angular, este sendo servido pelo nginx

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { environment } from '../environments/environment';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  production = environment.production;
+}
