b2c9b5bbed77cea5dce286a46c699bb14e02c3c7
Jefferson Boldrin Cardozo <jefferson.boldrin@gmail.com>
2018-04-04 12:04:22 -0300

    Re-implementa guarda de login

--- angular/src/app/app.routing.module.ts
+++ angular/src/app/app.routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 
+import { AuthGuard } from './guards/auth.guard';
+
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'conta', loadChildren: 'app/contas/contas.module#ContasModule' },
-    { path: 'eventos', loadChildren: 'app/eventos/eventos.module#EventosModule' },
+    { path: 'eventos',
+        loadChildren: 'app/eventos/eventos.module#EventosModule',
+        canActivate: [AuthGuard],
+        canLoad: [AuthGuard],
+    },
     { path: ':404', redirectTo: '', pathMatch: 'full' },
 ];
 
---
+++ angular/src/app/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
+
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanLoad {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  private verificarAcesso() {
+    if (localStorage.getItem('auth_token')) {
+      return true;
+    }
+
+    this.router.navigate(['/conta/login']);
+    return false;
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+
+    return this.verificarAcesso();
+  }
+
+  canLoad(
+    route: Route
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verificarAcesso();
+  }
+
+}
