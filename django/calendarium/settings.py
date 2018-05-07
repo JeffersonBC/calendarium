f23814fd0abfe089f77789246d2413807f675824
Jefferson Boldrin Cardozo <jefferson.boldrin@gmail.com>
2018-05-07 01:30:30 -0300

    Atualiza lista de CORS

--- django/calendarium/settings.py
+++ django/calendarium/settings.py
@@ -42,7 +42,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '35.196.7.65',
-    'calendarium.jeffersonbc.com',
+    'service.calendarium.jeffersonbc.com',
     'localhost',
 ]
 
@@ -187,9 +187,11 @@ JWT_AUTH = {
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
+    'calendarium.jeffersonbc.com',
     '35.196.7.65'
 )
 CORS_ORIGIN_REGEX_WHITELIST = (
     'localhost:4200',
+    'calendarium.jeffersonbc.com',
     '35.196.7.65'
 )
