b9ee85e58c3b50a720110eff020ce04684cfb6bb
JeffersonBC <jefferson.boldrin@gmail.com>
2018-06-08 18:08:27 -0300

    Host do Redis como variável de ambiente

--- django/calendarium/settings.py
+++ django/calendarium/settings.py
@@ -193,7 +193,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis', 6379)],
+            "hosts": [(get_site_var('REDIS_HOST'), 6379)],
         },
     },
 }
